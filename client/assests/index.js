0cca2f64aea6d21739209fcecee1869af77d17c9
laisjmvet <laisjmvet@gmail.com>
2023-06-12 19:11:25 +0100

    everthing is working, add variables in each fetch functions

--- client/assests/index.js
+++ client/assests/index.js
@@ -1,5 +1,4 @@
-async function randomQuestions(e) {  
-  e.preventDefault();   
+async function randomQuestions() {   
 
   const options = {
       method: "GET",
@@ -8,20 +7,26 @@ async function randomQuestions(e) {
       }
   }
   try {    
-    const response = await fetch(`http://localhost:3000/randomQuestions`, options);   
-    const questions = await response.json()  
+    const response = await fetch(`http://localhost:3000/randomQuestions`, options); 
+    const questions = await response.json(); 
+    
+    const q1 = questions[0];
+    const q2 = questions[1];
+    const q3 = questions[2];
+    const q4 = questions[3];
+    const q5 = questions[4];
+    const q6 = questions[5];
+    const q7 = questions[6];
+    const q8 = questions[7];
+    const q9 = questions[8];
+    const q10 = questions[9];    
 
-    const paintingImageURL = questions.imageUrl;
-    const choice1 = `${questions.name} - ${questions.author}`;   
-
-    console.log(questions)
   } catch (error) {
     console.log(error);
   }
 }  
 
-async function randomAnswers(e) {  
-  e.preventDefault();   
+async function randomAnswers() {   
 
   const options = {
       method: "GET",
@@ -30,18 +35,26 @@ async function randomAnswers(e) {
       }
   }
   try {    
-     
+         
     const response = await fetch(`http://localhost:3000/randomQuestions/randomAnswers`, options);
     const answers = await response.json()
-   
-    const choice2 = `${answers[0].name} - ${answers[0].author}`;
-    const choice3 = `${answers[1].name} - ${answers[1].author}`;
-    const choice4 = `${answers[2].name} - ${answers[2].author}`;
-    const choice5 = `${answers[3].name} - ${answers[3].author}`;
-    
+
+    const q1MultipleChoices = answers[0].wrongAuthors;
+    const q2MultipleChoices = answers[1].wrongAuthors;
+    const q3MultipleChoices = answers[2].wrongAuthors;
+    const q4MultipleChoices = answers[3].wrongAuthors;
+    const q5MultipleChoices = answers[4].wrongAuthors;
+    const q6MultipleChoices = answers[5].wrongAuthors;
+    const q7MultipleChoices = answers[6].wrongAuthors;
+    const q8MultipleChoices = answers[7].wrongAuthors;
+    const q9MultipleChoices = answers[8].wrongAuthors;
+    const q10MultipleChoices = answers[9].wrongAuthors;
+
   } catch (error) {
     console.log(error);
   }
 }  
 
+randomQuestions();
+randomAnswers();
 
