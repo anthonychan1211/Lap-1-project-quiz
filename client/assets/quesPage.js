09b0b7ca222f01ac5f8cfb2117e44309e5186862
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-06-13 17:06:44 +0100

    added next ques button, displays result

--- client/assets/quesPage.js
+++ client/assets/quesPage.js
@@ -22,11 +22,13 @@ const displayTopBar = (quesNum, wrongGuess,score) => {
     4 - wrongGuess
   )}`;
   topBar.appendChild(chancesLeft);
-};
+}
 
 const displayQues = async (quesNum) => {
   questionSection.textContent=""
   answerSection.textContent=""
+  resultsSection.textContent=""
+
   const data = await fetchData();
   const quesImage = document.createElement("img");
   let correctAuthor = data[quesNum-1].author
@@ -54,47 +56,88 @@ const displayQues = async (quesNum) => {
         choice.disabled=true
         //choice.style.backgroundColor=red
       }
-      results(correct)
+      checkAnswer(correct)
     })
   }
 
 };
 
-const results = (correct) => {
+const checkAnswer = (correct) => {
+  resultsSection.textContent=""
   quesImage = document.querySelector('.image-container img')
-  if (correct==true && wrongGuess<4){
-    alert('correct')
+  if (correct==true && wrongGuess<5){
+    // alert('correct')
+    results('correct')
     quesNum++
-    runGame(quesNum,0)
+    //runGame(quesNum,0)
   } else {
     wrongGuess++
     topBar.childNodes[2].textContent=`${"x ".repeat(wrongGuess)}${"o ".repeat(4 - wrongGuess)}`
     if (wrongGuess==4){
-      alert ('game over')
+      // alert ('game over')
       quesImage.classList.remove("guess3")
       questionSection.style.objectFit='none'
       wrongGuess=0
-      runGame(quesNum,wrongGuess)
+      results('fail')
       //next question button
     } else {
       quesImage.className=`guess${wrongGuess}`
-      alert('incorrect')
+      // alert('incorrect')
+      results('incorrect')
     }
   }
 }
 
+const results = (result) => {
+  console.log(wrongGuess)
+  const resultsText = document.createElement('p')
+  if (result=='correct'){
+    resultsText.textContent="Correct!"
+    resultsSection.appendChild(resultsText)
+    if (quesNum<5){
+      nextQues()
+    }
+  } else if (result=='incorrect'){
+    resultsText.textContent=`Incorrect! You have ${4-wrongGuess} chance${((4-wrongGuess)==1?``: `s`)} left`
+    resultsSection.appendChild(resultsText)
+  } else if (result=='fail'){      
+      resultsText.textContent="Incorrect! You have no chances left."
+      resultsSection.appendChild(resultsText)
+    if (quesNum<5){
+      nextQues()
+    }
+  } 
+  if (quesNum==5) {
+    answerSection.childNodes.forEach(button => button.disabled=true)
+    finishGame()
+  }
+}
+
+const nextQues = () => {
+  const nextQButton = document.createElement('button')
+  nextQButton.textContent = "Next question"
+  resultsSection.appendChild(nextQButton)
+  nextQButton.addEventListener('click', () => {
+  runGame(quesNum,0,score)})
+  answerSection.childNodes.forEach(button => button.disabled=true)
+}
+
+const finishGame = () => {
+  alert ('you finished the game')
+}
+
 const runGame = (quesNum,wrongGuess,score) => {
+  wrongGuess=0
   if (quesNum<6) {
     displayTopBar(quesNum, wrongGuess,score);
     displayQues(quesNum)
-  } else {
-    alert ('you finished the game')
   }
 }
 
 const topBar = document.querySelector(".topBar");
 const questionSection = document.querySelector(".image-container");
 const answerSection = document.querySelector(".answers");
+const resultsSection = document.querySelector(".results")
 
 let quesNum = 1;
 let wrongGuess = 0;
