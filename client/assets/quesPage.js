267cdcfa2cf4d5bf3cc3874096d7c4b75e859c86
anthonychan1211 <anthonychan1211@gmail.com>
2023-06-14 09:04:55 +0100

    newer

--- client/assets/quesPage.js
+++ client/assets/quesPage.js
@@ -1,17 +1,24 @@
 const fetchData = async (difficulty) => {
+<<<<<<< HEAD
   const res = await fetch(`http://localhost:3000/randomQuestions/${difficulty}`);
   const data = await res.json();  
+=======
+  const res = await fetch(
+    `http://localhost:3000/randomQuestions/${difficulty}`
+  );
+  const data = await res.json();
+>>>>>>> d115e4b (newer)
   return data;
 };
 
-const displayTopBar = (quesNum, wrongGuess,score) => {
-  topBar.textContent=""
+const displayTopBar = (quesNum, wrongGuess, score) => {
+  topBar.textContent = "";
   const quesProgress = document.createElement("p");
   quesProgress.textContent = `Q${quesNum}/5`;
   topBar.appendChild(quesProgress);
-  
-  const scoreBar = document.createElement("p")
-  scoreBar.textContent = score
+
+  const scoreBar = document.createElement("p");
+  scoreBar.textContent = score;
 
   const instruction = document.createElement("p");
   instruction.textContent = "Select the author of the painting";
@@ -28,9 +35,19 @@ const displayTopBar = (quesNum, wrongGuess,score) => {
 let data = null;
 let fetchStatus = false;
 const displayQues = async (quesNum) => {
+<<<<<<< HEAD
   
   questionSection.textContent="";
   answerSection.textContent="" ;   
+=======
+  questionSection.textContent = "";
+  answerSection.textContent = "";
+
+  const quesImage = document.createElement("img");
+  quesImage.id = "painting";
+
+  let correctAuthor = data[quesNum - 1].author;
+>>>>>>> d115e4b (newer)
 
   if(!fetchStatus) {
     data = await fetchData("easy");
@@ -42,7 +59,10 @@ const displayQues = async (quesNum) => {
   quesImage.id = "painting"  
   quesImage.src = data[quesNum - 1].imageUrl;
   quesImage.alt = "Q1 picture";
+<<<<<<< HEAD
  
+=======
+>>>>>>> d115e4b (newer)
   questionSection.appendChild(quesImage);
 
   let randomizeChoices = data[quesNum - 1].wrongAuthors;
@@ -53,6 +73,7 @@ const displayQues = async (quesNum) => {
 
   for (let i = 0; i < 5; i++) {
     const choice = document.createElement("button");
+<<<<<<< HEAD
     choice.textContent = randomizeChoices[i];    
     answerSection.appendChild(choice);    
     
@@ -66,10 +87,27 @@ const displayQues = async (quesNum) => {
         zoomLevel = 10;        
         runGame(quesNum,wrongGuess);
 
-      } else {
-        wrongGuess++
-        topBar.childNodes[2].textContent=`${"x ".repeat(wrongGuess)}${"o ".repeat(4 - wrongGuess)}`        
+=======
+    choice.textContent = randomizeChoices[i];
+    choice;
+    answerSection.appendChild(choice);
 
+    choice.addEventListener("click", () => {
+      if (choice.textContent == correctAuthor) {
+        alert("correct");
+        quesNum++;
+        wrongGuess = 0;
+        painting.style.transform = `scale(${1})`;
+        zoomLevel = 10;
+        runGame(quesNum, wrongGuess);
+>>>>>>> d115e4b (newer)
+      } else {
+        wrongGuess++;
+        topBar.childNodes[2].textContent = `${"x ".repeat(
+          wrongGuess
+        )}${"o ".repeat(4 - wrongGuess)}`;
+
+<<<<<<< HEAD
         if (wrongGuess==4){
           alert ('Out of tries');
           zoomLevel = 10;
@@ -78,12 +116,39 @@ const displayQues = async (quesNum) => {
         } else {
           zoomOut();          
           alert('incorrect');
+=======
+        if (wrongGuess == 4) {
+          alert("Out of tries");
+          // quesImage.classList.remove("guess3")
+          // questionSection.style.objectFit=none
+          zoomLevel = 10;
+          painting.style.transform = `scale(${1})`;
+        } else {
+          zoomOut();
+          // quesImage.className=`guess${wrongGuess}`
+          alert("incorrect");
+>>>>>>> d115e4b (newer)
         }
       }
-    })
+    });
+  }
+};
+fetchData("hard").then((data) => {
+  console.log(data);
+});
+//need to fix the run game count
+const runGame = (quesNum, wrongGuess, score) => {
+  // const data = await fetchData("hard");
+  if (quesNum < 6) {
+    displayTopBar(quesNum, wrongGuess, score);
+    displayQues(quesNum);
+  } else {
+    //cant finish the game, you need to go to next question
+    alert("you finished the game");
   }
 };
 
+<<<<<<< HEAD
 const runGame = (quesNum,wrongGuess,score) => {  
   
   if (quesNum<6) {
@@ -120,6 +185,13 @@ const zoomOut = () => {
   painting.style.transform = `scale(${zoomLevel})`
 
 }
+=======
+const zoomOut = () => {
+  const painting = document.getElementById("painting");
+  zoomLevel -= 2;
+  painting.style.transform = `scale(${zoomLevel})`;
+};
+>>>>>>> d115e4b (newer)
 
 const topBar = document.querySelector(".topBar");
 const questionSection = document.querySelector(".image-container");
@@ -129,4 +201,8 @@ let quesNum = 1;
 let wrongGuess = 0;
 let score = 0;
 
+<<<<<<< HEAD
 runGame(quesNum,wrongGuess,score);
+=======
+runGame(quesNum, wrongGuess, score);
+>>>>>>> d115e4b (newer)
