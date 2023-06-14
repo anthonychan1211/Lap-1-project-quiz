f2bb0f8798aba59b4133b91edc953507e2c15497
Jacob <Jacobabrooks04@gmail.com>
2023-06-14 11:25:34 +0100

    Now sends to different difficulty when button clicked

--- client/Homepage/homePage.js
+++ client/Homepage/homePage.js
@@ -1,16 +1,18 @@
 const easyButton = () =>{
     const start = document.querySelector("#easy")
     start.addEventListener('click', () => {
-        let difficulty = "easy"
-        window.open(`../quesPage/${difficulty}.html`,"_self")
+        
+        localStorage.setItem("difficulty","easy")
+        window.open("../QuestionPage/quesPage.html","_self")
     })
 }
 
 const hardButton = () =>{
     const start = document.querySelector("#hard")
     start.addEventListener('click', () => {
-        let difficulty = "hard"
-        window.open(`../quesPage.html`,"_self")
+        
+        localStorage.setItem("difficulty","hard")
+        window.open(`../QuestionPage/quesPage.html`,"_self")
     })
 }
 
