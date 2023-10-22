84e08073cdbf33bd5710e13e0818f419a72a78c2
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-10-22 13:46:15 +0100

    final

--- server/index.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const app = require("./app");
-const port = process.env.PORT;
-console.log(port);
-app.listen(port, () => {
-  console.log(`API listening on port ${port}.`);
-});
